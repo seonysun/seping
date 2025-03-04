a30c254877392e3037019ba169afdfa4690c4890
seonysun <csh99240@naver.com>
2025-03-05 02:31:56 +0900

    feat: toggleButton 컴포넌트 생성

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { Favorite, User } from '../assets/icons';
 import Button from '../components/Button/Button';
+import ToggleButton from '../components/Button/ToggleButton';
 
 const USER_ICONS = [
   { src: Favorite, alt: 'Favorite' },
@@ -11,13 +12,17 @@ function Header() {
   return (
     <header className="flex h-[88px] items-center justify-between border px-4 md:px-[10%]">
       <Link to="/">seping</Link>
-      <nav className="flex gap-3">
+      <nav className="flex gap-2">
         {USER_ICONS.map((icon) => (
           <button key={icon.alt} type="button">
             <img src={icon.src} alt={icon.alt} />
           </button>
         ))}
         <Button text="로그인" color="btn-purple" font="white" />
+        <div className="flex flex-col items-center justify-center gap-1 text-xs">
+          다크모드
+          <ToggleButton />
+        </div>
       </nav>
     </header>
   );
