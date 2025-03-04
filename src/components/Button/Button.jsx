9734217de55815747385a4869112062677d6a9ea
seonysun <csh99240@naver.com>
2025-03-05 03:16:15 +0900

    design: NotFound 페이지 버튼 css 수정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,14 +4,14 @@ const BTN_FONT = {
 };
 const BTN_SIZE = {
   high: 'h-[56px] w-[150px]',
-  low: 'h-[48px] w-[162px] ',
+  low: 'h-[48px] w-[162px]',
 };
 
 function Button({ color, font = 'black', size, text = 'Button' }) {
   return (
     <button
       type="button"
-      className={`flex items-center rounded-lg border p-2 ${color} ${BTN_FONT[font]} ${BTN_SIZE[size] || size}`}
+      className={`rounded-lg border p-2 ${color} ${BTN_FONT[font]} ${BTN_SIZE[size] || size}`}
     >
       {text}
     </button>
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+/** button */
+export { default as Button } from './Button/Button';
+export { default as ToggleButton } from './Button/ToggleButton';
+
+/** input */
+export { default as SearchInput } from './Input/SearchInput';
+export { default as FilterInput } from './Input/FilterInput';
+export { default as DropdownInput } from './Input/DropdownInput';
