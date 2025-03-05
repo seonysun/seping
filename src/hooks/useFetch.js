e7a9c2652905a94ef4569e1747ef8a3847550c43
seonysun <csh99240@naver.com>
2025-03-05 15:39:14 +0900

    refactor: useFetch 내부 최적화 작업(useCallback 처리)

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,30 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { instance } from '../utils/api/instance';
 
 const useFetch = (url, params = {}) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    async function getData() {
-      setLoading(true);
-
-      try {
-        const response = await instance.get(url, { params });
-        setData(response.data);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
+  const getData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await instance.get(url, { params });
+      setData(response.data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
     }
-
-    getData();
   }, [url, JSON.stringify(params)]);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
 
   useEffect(() => {
     if (data) console.log(data);
   }, [data]);
+
   return { data, loading };
 };
 
