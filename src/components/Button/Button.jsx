053a7303b495b82079f568564d277f9fed604dd1
seonysun <csh99240@naver.com>
2025-03-08 22:43:06 +0900

    feat: react-icons 라이브러리 추가

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,13 @@
 const BTN_FONT = {
-  white: 'border-white text-white',
-  black: 'border-black text-black',
+  white: 'text-white',
+  black: 'border border-black text-black',
 };
 const BTN_SIZE = {
   high: 'h-[56px] w-[150px]',
   low: 'h-[48px] w-[162px]',
 };
 
-function Button({ color, font = 'black', size, text = 'Button', onClick }) {
+function Button({ color, font = 'white', size, text = 'Button', onClick }) {
   return (
     <button
       type="button"
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,47 +1,41 @@
 import { useState } from 'react';
+import { FaRegHeart, FaUser } from 'react-icons/fa';
+import { RxHamburgerMenu } from 'react-icons/rx';
 import { Link } from 'react-router-dom';
 import SideModal from './SideModal';
-import { BurgerIcon, Favorites, UserIcon } from '../assets/icons';
 import { Button, ToggleButton } from '../components';
-import useTheme from '../hooks/useTheme';
 
 function UserNav() {
-  const { theme } = useTheme();
   const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <nav className="flex gap-2">
-      <Favorites color={theme === 'light' ? 'black' : 'white'} />
-      {isLogin && <UserIcon color={theme === 'light' ? 'black' : 'white'} />}
+    <nav className="flex items-center gap-3">
+      <FaRegHeart size="22" />
+      {isLogin && <FaUser size="22" />}
       <Button
         text={isLogin ? '로그아웃' : '로그인'}
         color="btn-purple"
-        font="white"
+        size="text-sm"
         onClick={() => {
           setIsLogin((prev) => !prev);
         }}
       />
-      <div className="flex flex-col items-center justify-center gap-1 text-xs">
-        다크모드
-        <ToggleButton />
-      </div>
+      <ToggleButton />
     </nav>
   );
 }
 
 function Header() {
   const [isSideOpen, setIsSideOpen] = useState(false);
-  const { theme } = useTheme();
 
   return (
     <header className="flex h-[88px] items-center border-b px-4 md:px-[10%]">
       {isSideOpen && <SideModal setIsSideOpen={setIsSideOpen} />}
-      <BurgerIcon
-        color={theme === 'light' ? 'black' : 'white'}
+      <RxHamburgerMenu
+        size="28"
         onClick={() => setIsSideOpen((prev) => !prev)}
       />
-
-      <Link to="/" className="ml-2 flex-1 justify-start">
+      <Link to="/" className="ml-4 flex-1 justify-start">
         seping
       </Link>
 
