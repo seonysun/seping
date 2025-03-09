be82768faaf97806c48b3aa5dba17261bdd5f7ec
seonysun <csh99240@naver.com>
2025-03-09 21:43:14 +0900

    design: swiper 잠금화면 css 수정

--- src/pages/Home/LockScreen.jsx
+++ src/pages/Home/LockScreen.jsx
@@ -1,14 +1,13 @@
 import { LuChevronUp } from 'react-icons/lu';
 import { useNavigate } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Header, Navbar } from '../../layout';
+import { Navbar } from '../../layout';
 import ListFallBack from '../List/ListFallback';
 import 'swiper/css';
 
 function SwipeContent() {
   return (
-    <div className="flex h-svh flex-col">
-      <Header />
+    <div className="absolute flex size-full flex-col">
       <ListFallBack num={6} />
       <Navbar />
     </div>
@@ -19,24 +18,25 @@ function LockScreen() {
   const navigate = useNavigate();
 
   return (
-    <Swiper
-      direction="vertical"
-      // allowSlidePrev={false}
-      onReachEnd={() => navigate('home')}
-      threshold={50}
-      style={{ height: '100vh' }}
-    >
-      <SwiperSlide className="flex flex-col items-center justify-around bg-[#ac72b2]">
-        <p className="text-4xl text-white">seping.</p>
-        <div className="animate-spark text-[#553858]">
-          <LuChevronUp size="80" />
-          <p className="text-xl">시작하기</p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <SwipeContent />
-      </SwiperSlide>
-    </Swiper>
+    <>
+      <SwipeContent />
+      <Swiper
+        direction="vertical"
+        allowSlidePrev={false}
+        onReachEnd={() => navigate('home')}
+        threshold={50}
+        style={{ height: '100vh' }}
+      >
+        <SwiperSlide className="flex flex-col items-center justify-around bg-[#ac72b2]">
+          <p className="text-4xl text-white">seping.</p>
+          <div className="animate-spark cursor-pointer text-[#553858]">
+            <LuChevronUp size="80" />
+            <p className="text-xl">시작하기</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide />
+      </Swiper>
+    </>
   );
 }
 
