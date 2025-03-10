926af139f3c15de6541baab319aeab125bb466ff
seonysun <csh99240@naver.com>
2025-03-10 23:24:33 +0900

    feat: 좋아요 로컬스토리지에 저장 기능 추가

--- src/redux/Slice/likeSlice.js
+++ src/redux/Slice/likeSlice.js
@@ -6,10 +6,14 @@ export const likeSlice = createSlice({
   reducers: {
     addLike(state, action) {
       state.push(action.payload);
+
+      localStorage.setItem('like', JSON.stringify(state));
     },
     unLike(state, action) {
       const index = state.indexOf(action.payload);
       if (index !== -1) state.splice(index, 1);
+
+      localStorage.setItem('like', JSON.stringify(state));
     },
   },
 });
