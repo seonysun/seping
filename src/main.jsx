341f3efa4024e5b70f78bb1b9d349f63a1bdda37
seonysun <csh99240@naver.com>
2025-03-18 14:38:34 +0900

    chore: react-query 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
@@ -7,12 +8,16 @@ import { store } from './redux/store';
 import { router } from './router';
 import './styles/main.css';
 
+const query = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <QueryClientProvider client={query}>
+        <ThemeProvider>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
