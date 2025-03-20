a4bf493ce83aa5c61189055ed38bb9674dc1af3b
seonysun <csh99240@naver.com>
2025-03-20 13:55:12 +0900

    chore: supabase 설치

---
+++ src/supabase/auth/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-cycle */
+import { useAuth } from './useAuth';
+import { useEmailAuth } from './useEmail.auth';
+import { useOAuth } from './useOauth.auth';
+
+export const useSupabaseAuth = () => {
+  const { getUserInfo, logout } = useAuth();
+  const { login, signUp } = useEmailAuth();
+  const { loginWithGoogle, loginWithKakao } = useOAuth();
+
+  return {
+    login,
+    signUp,
+    getUserInfo,
+    logout,
+    loginWithKakao,
+    loginWithGoogle,
+  };
+};
