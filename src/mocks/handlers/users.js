4162b56ef6ebb27a3dc29dadea924d20013baf7b
seonysun <csh99240@naver.com>
2025-03-26 17:00:36 +0900

    fix: api 오타 수정

--- src/mocks/handlers/users.js
+++ src/mocks/handlers/users.js
@@ -2,7 +2,7 @@ import { delay, http, HttpResponse } from 'msw';
 import { users } from '../datas/users';
 
 const userHandler = [
-  http.get('/api/posts', async () => {
+  http.get('/api/users', async () => {
     await delay(1000);
     return HttpResponse.json(users, { status: 200 });
   }),
