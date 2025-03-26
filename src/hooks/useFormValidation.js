d2feb5ee616c9216b0e2276ac3c1c78b7e14ad57
seonysun <csh99240@naver.com>
2025-03-26 13:08:41 +0900

    feat: 로그인화면 초기 입력시 검사하지 않도록 수정

--- src/hooks/useFormValidation.js
+++ src/hooks/useFormValidation.js
@@ -5,7 +5,7 @@ const useFormValidation = (values, setError, type) => {
     const errors = {};
 
     if (!values.email) {
-      errors.email = '이메일을 입력하세요.';
+      // errors.email = '이메일을 입력하세요.';
     } else if (
       !/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(values.email)
     ) {
@@ -13,20 +13,20 @@ const useFormValidation = (values, setError, type) => {
     }
 
     if (!values.password) {
-      errors.password = '비밀번호를 입력하세요.';
+      // errors.password = '비밀번호를 입력하세요.';
     } else if (values.password.length < 8) {
       errors.password = '비밀번호는 최소 8자 이상이어야 합니다.';
     }
 
     if (type === 'signup') {
       if (!values.name) {
-        errors.name = '이름을 입력하세요.';
+        // errors.name = '이름을 입력하세요.';
       } else if (!/^[a-zA-Z0-9가-힣]{2,8}$/.test(values.name)) {
         errors.name = '2~8자, 숫자, 한글, 영어로만 가능합니다.';
       }
 
       if (!values.confirmPassword) {
-        errors.confirmPassword = '비밀번호를 다시 입력하세요.';
+        // errors.confirmPassword = '비밀번호를 다시 입력하세요.';
       } else if (values.password !== values.confirmPassword) {
         errors.confirmPassword = '비밀번호가 일치하지 않습니다.';
       }
