5d9ec5a4eb73d895f05aad41654db4b4660c9150
seonysun <csh99240@naver.com>
2025-04-10 14:01:08 +0900

    feat: 웹 최적화를 위한 avif, webP 포맷 추가

--- src/pages/Home/LockScreen.jsx
+++ src/pages/Home/LockScreen.jsx
@@ -32,11 +32,15 @@ function LockScreen() {
           <div className="flex h-screen flex-col items-center justify-around">
             <div className="flex flex-col items-center gap-6">
               <p className="text-4xl text-white">SpeakUp</p>
-              <img
-                src="/speakUp.png"
-                alt="speakup"
-                className="h-auto w-[150px]"
-              />
+              <picture>
+                <source srcSet="/speakUp.avif" type="image/avif" />
+                <source srcSet="/speakUp.webp" type="image/webp" />
+                <img
+                  src="/speakUp.png"
+                  alt="speakup"
+                  className="h-auto w-[160px]"
+                />
+              </picture>
             </div>
             <div className="animate-spark cursor-pointer text-[#336380]">
               <Icon icon="LuChevronUp" size="80" />
