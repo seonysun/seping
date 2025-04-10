cd4acde7773ccdd7b1b1cb9ba84b357e86e43b36
seonysun <csh99240@naver.com>
2025-04-10 13:22:08 +0900

    feat: favicon 변경

--- src/pages/Home/LockScreen.jsx
+++ src/pages/Home/LockScreen.jsx
@@ -28,10 +28,17 @@ function LockScreen() {
         threshold={50}
         style={{ height: '100vh', zIndex: 50 }}
       >
-        <SwiperSlide className="bg-[#ac72b2]">
+        <SwiperSlide className="bg-[#B9C3CC]">
           <div className="flex h-screen flex-col items-center justify-around">
-            <p className="text-4xl text-white">seping.</p>
-            <div className="animate-spark cursor-pointer text-[#553858]">
+            <div className="flex flex-col items-center gap-6">
+              <p className="text-4xl text-white">SpeakUp</p>
+              <img
+                src="/speakUp.png"
+                alt="speakup"
+                className="h-auto w-[150px]"
+              />
+            </div>
+            <div className="animate-spark cursor-pointer text-[#336380]">
               <Icon icon="LuChevronUp" size="80" />
               <p className="text-xl">시작하기</p>
             </div>
