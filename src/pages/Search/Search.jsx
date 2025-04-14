196263919d2f4ecebdc338fb431494fe588bc90a
seonysun <csh99240@naver.com>
2025-04-14 15:39:58 +0900

    feat: search 컴포넌트 수정

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,35 +1,65 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { useSearchParams } from 'react-router-dom';
-import CardSkeleton from '../../components/Card/CardSkeleton';
-import VideoCard from '../../components/Card/VideoCard';
+import ListSkeleton from '../../components/Card/ListSkeleton';
+import YoutubeCard from '../../components/Card/YoutubeCard';
 import SearchInput from '../../components/Input/SearchInput';
-import { MAX_LIST_LENGTH } from '../../constants/uiData';
+import { MAX_LIST_LENGTH, SKELETON } from '../../constants/uiData';
+import useIntersectionObserver from '../../hooks/useIntersectionObserver';
 import videoOptions from '../../utils/api/videoOptions';
 
 function Search() {
   const [searchParams] = useSearchParams();
-  const input = searchParams.get('input');
-  const { data, isLoading } = useQuery(videoOptions.searchList(input));
+  const query = searchParams.get('input');
 
-  if (isLoading) return <CardSkeleton num={MAX_LIST_LENGTH.SEARCH.ITEMS} />;
+  const { data, isLoading, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery(videoOptions.playSearch(query));
+
+  const observerRef = useIntersectionObserver({ hasNextPage, fetchNextPage });
+
+  const total = data?.pages?.[0]?.total;
+  const formattedTotal = total ? total.toLocaleString() : '0';
 
   return (
-    <main className="px-4 md:px-[10%]">
+    <main>
       <SearchInput />
-      <p className="mt-2 text-lg">
-        <span className="bg-main rounded-lg px-3 py-2 text-xl text-white">
-          {input}
-        </span>
-        &nbsp;검색 결과 :{' '}
-        <span className="text-main text-xl">{data.total}</span>개
-      </p>
-      <ul className="mt-6 flex flex-wrap justify-center gap-4">
-        {data?.total === 0 ? (
-          <p>검색 결과가 없습니다</p>
-        ) : (
-          data.products.map((item) => <VideoCard key={item.id} item={item} />)
-        )}
-      </ul>
+      {isLoading ? (
+        <ListSkeleton
+          num={MAX_LIST_LENGTH.SEARCH.ITEMS}
+          size={SKELETON.responsive23}
+        />
+      ) : (
+        <div>
+          <p className="mt-3 text-lg">
+            <span className="bg-main rounded-lg px-3 py-2 text-xl text-white">
+              {query}
+            </span>
+            &nbsp;검색 결과 :{' '}
+            <span className="text-main text-xl">{formattedTotal}</span>개
+          </p>
+          {!total ? (
+            <p className="mt-4">검색 결과가 없습니다</p>
+          ) : (
+            <ul className="mt-6 flex flex-wrap justify-center gap-4">
+              {data?.pages.flatMap((page) =>
+                page.items.map((item) => (
+                  <YoutubeCard
+                    key={item.id.videoId}
+                    id={item.id.videoId}
+                    item={item.snippet}
+                  />
+                )),
+              )}
+              {isFetchingNextPage && (
+                <ListSkeleton
+                  num={MAX_LIST_LENGTH.VIDEO.ITEMS}
+                  size={SKELETON.responsive23}
+                />
+              )}
+            </ul>
+          )}
+          <div ref={observerRef} className="h-10" />
+        </div>
+      )}
     </main>
   );
 }
